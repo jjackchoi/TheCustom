df55696eb9d3caa60f127dc635e4ffa596374cd3
jjackchoi <jackchoi0325@gmail.com>
2023-12-14 02:35:06 +0900

    first commit

---
+++ src/test/java/com/customEdu/CustomEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
