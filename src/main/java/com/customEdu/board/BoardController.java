b4e431c6a0d78f8e216d66a2087ecc24e0ab10c8
jjackchoi <jackchoi0325@gmail.com>
2023-12-23 02:00:51 +0900

    글쓰기 기능 구현

--- src/main/java/com/customEdu/board/BoardController.java
+++ src/main/java/com/customEdu/board/BoardController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class BoardController {
     final BoardService boardService;
 
     // 게시글 리스트 페이지
-    @GetMapping("/board/list.do")
+    @GetMapping("/")
     public String openBoardList(Model model){
         List<BoardResponse> boards = boardService.findAllBoard();
         model.addAttribute("boards", boards);
@@ -24,11 +25,17 @@ public class BoardController {
     }
     // 게시글 작성 페이지
     @GetMapping("/board/write.do")
-    public String openBoardWrite(@RequestParam(value = "seq", required = false) final Long seq, Model model){
+    public String openBoardWrite(@RequestParam(value = "boardSeq", required = false) final Long seq, Model model){
         if (seq != null){
             BoardResponse board = boardService.findBoardById(seq);
             model.addAttribute("board", board);
         }
         return "board/write";
+    }
+
+    @PostMapping("/board/save.do")
+    public String saveBoard(final BoardRequest params){
+        boardService.saveBoard(params);
+        return "redirect:/";
     }
 }
